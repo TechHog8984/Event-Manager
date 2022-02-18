cabf9808b735730956fb871941f20bac6acfe018
TechHog <77560013+TechHog8984@users.noreply.github.com>
2022-02-18 22:14:23 +0000

    Update src.lua

--- src.lua
+++ src.lua
@@ -282,36 +282,4 @@ local function CreateEvent(Info)
 	return Event;
 end;
 
-do --demonstration
-	local Event = CreateEvent{Name = 'Orangutan'};
-
-	local Connection1 = Event:Connect(function(Key, ...)
-		if Key == 'Key' then
-			print('Passed: ', ...);
-		else
-			print('Invalid key.');
-		end;
-	end);
-	Event:Fire('Key', 'Execute', 'Permission=true');
-	Event:Fire('bruh', 'Execute');
-	Connection1:Disconnect();
-	Event:Fire('Key', 'Clear', 'All');
-
-	Event:Connect(function(...)
-		print('Con1:', ...);
-	end);
-	Event:Connect(function(...)
-		print('Con2:', ...);
-	end);
-	Event:Connect(function(...)
-		print('Con3:', ...);
-	end);
-
-	Event:Fire('Push', 'Array1', 'Value6');
-	Event:Fire('Pop', 'Array2', 'Value5');
-
-	Event:DisconnectAll();
-
-	Event:Fire('Pull', 'Array2', 'Value3');
-	Event:Fire('Get', 'Array2', 'Value9');
-end;
+return CreateEvent;
